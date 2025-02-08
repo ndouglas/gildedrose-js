279a8b03ea8e4edfbb7210c4bf40d5b5a8cefafe
Nathan Douglas <github@darkdell.net>
2025-02-07 21:38:08 -0500

    Extract method: didUpdateSellIn().

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -37,29 +37,33 @@ class Shop {
         }
       }
       this.updateSellIn(item);
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
+      this.didUpdateSellIn(item);
     }
   }
 
   updateSellIn(item) {
     if (item.name != 'Sulfuras, Hand of Ragnaros') {
       item.sellIn = item.sellIn - 1;
+    }
+  }
+
+  didUpdateSellIn(item) {
+    if (item.sellIn < 0) {
+      if (item.name != 'Aged Brie') {
+        if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (item.quality > 0) {
+            if (item.name != 'Sulfuras, Hand of Ragnaros') {
+              item.quality = item.quality - 1;
+            }
+          }
+        } else {
+          item.quality = item.quality - item.quality;
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+      }
     }
   }
 }
