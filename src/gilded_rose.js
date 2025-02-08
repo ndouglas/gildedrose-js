e1cfbb978fe2a65def6daae1af69b1de02df5fb9
Nathan Douglas <github@darkdell.net>
2025-02-07 21:51:07 -0500

    Shop::didUpdateSellIn() -> SellInUpdater::didUpdate().

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -11,42 +11,18 @@ class Shop {
   constructor(items = []) {
     this.items = items;
     this.qualityUpdater = new QualityUpdater();
+    this.sellInUpdater = new SellInUpdater();
   }
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
       this.qualityUpdater.update(item);
-      this.updateSellIn(item);
-      this.didUpdateSellIn(item);
+      this.sellInUpdater.update(item);
+      this.sellInUpdater.didUpdate(item);
     }
   }
 
-  updateSellIn(item) {
-    if (item.name != 'Sulfuras, Hand of Ragnaros') {
-      item.sellIn = item.sellIn - 1;
-    }
-  }
-
-  didUpdateSellIn(item) {
-    if (item.sellIn < 0) {
-      if (item.name != 'Aged Brie') {
-        if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.quality > 0) {
-            if (item.name != 'Sulfuras, Hand of Ragnaros') {
-              item.quality = item.quality - 1;
-            }
-          }
-        } else {
-          item.quality = item.quality - item.quality;
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
-    }
-  }
 }
 
 class QualityUpdater {
@@ -72,6 +48,37 @@ class QualityUpdater {
               item.quality = item.quality + 1;
             }
           }
+        }
+      }
+    }
+  }
+}
+
+class SellInUpdater {
+  constructor() {
+  }
+
+  update(item) {
+    if (item.name != 'Sulfuras, Hand of Ragnaros') {
+      item.sellIn = item.sellIn - 1;
+    }
+  }
+
+  didUpdate(item) {
+    if (item.sellIn < 0) {
+      if (item.name != 'Aged Brie') {
+        if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (item.quality > 0) {
+            if (item.name != 'Sulfuras, Hand of Ragnaros') {
+              item.quality = item.quality - 1;
+            }
+          }
+        } else {
+          item.quality = item.quality - item.quality;
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
         }
       }
     }
