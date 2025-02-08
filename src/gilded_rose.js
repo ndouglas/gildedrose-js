ddb90b7d4d245a170f77dc1194e0eeaaa9ae5923
Nathan Douglas <github@darkdell.net>
2025-02-08 00:02:18 -0500

    use pre-defined names.

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -124,4 +124,5 @@ expirers[types.backstagePasses] = new BackstagePassesExpirer();
 module.exports = {
   Item,
   Shop,
+  types,
 }
--- test/texttest_fixture.js
+++ test/texttest_fixture.js
@@ -1,16 +1,16 @@
 
-const { Shop, Item } = require("../src/gilded_rose");
+const { Shop, Item, types } = require("../src/gilded_rose");
 
 const items = [
   new Item("+5 Dexterity Vest", 10, 20),
-  new Item("Aged Brie", 2, 0),
+  new Item(types.agedBrie, 2, 0),
   new Item("Elixir of the Mongoose", 5, 7),
-  new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-  new Item("Conjured Mana Cake", 3, 6),
+  new Item(types.sulfuras, 0, 80),
+  new Item(types.sulfuras, -1, 80),
+  new Item(types.backstagePasses, 15, 20),
+  new Item(types.backstagePasses, 10, 49),
+  new Item(types.backstagePasses, 5, 49),
+  new Item(types.conjured, 3, 6),
 ];
 
 const days = Number(process.argv[2]) || 2;
